f9e6df2adde054b826f8e36ad100f75e0c5a94c8
ElisabethKloren <ElisabethKloren@users.noreply.github.com>
2022-10-13 15:58:08 +0000

    Initialize new ReSpec repository
    Reason: run setup.yaml after clone by ElisabethKloren

---
+++ docs/bim-ondergrond/config.js
@@ -0,0 +1,15 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "bim-ondergrond/bim-ondergrond",
+  specType: "technische documentatie", // vrije tekst, kleine letter
+  imprint: "crow",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Elisabeth de Vries",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/bim-ondergrond",
+  license: "undefined",
+};
